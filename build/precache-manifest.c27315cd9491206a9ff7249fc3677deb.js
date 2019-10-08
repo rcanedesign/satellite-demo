313b7cd10cebb5a0ddd28761f1f52b593ac50423
athurber <7492930+athurber@users.noreply.github.com>
2019-10-07 22:15:05 -0700

    init react project

---
+++ build/precache-manifest.c27315cd9491206a9ff7249fc3677deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f2fd419d911e51bd78ee1cb577e051",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41fa228312e352ec495b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e00ea407b7f50d94c6dc",
+    "url": "/static/js/2.1cc6b7df.chunk.js"
+  },
+  {
+    "revision": "41fa228312e352ec495b",
+    "url": "/static/js/main.906e6aac.chunk.js"
+  },
+  {
+    "revision": "ae6c75b03bda3ee53ea9",
+    "url": "/static/js/runtime-main.c52f5233.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
